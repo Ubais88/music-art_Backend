3434e3dfc4036e5c2b7402c7a1f13ff584813ad0
Ubais88 <moubais2143@gmail.com>
2024-03-23 17:34:19 +0530

    user model added

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value.length >= 5;
+        },
+        message: "Password must be at least 5 characters long",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
