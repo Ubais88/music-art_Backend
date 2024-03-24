6ea995caba3da0e3a032e65ebb20fb2c135f6dbc
Ubais88 <moubais2143@gmail.com>
2024-03-24 10:29:26 +0530

    login controller added

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+
 // Signup Controller for Registering USers
 exports.signup = async (req, res) => {
   try {
@@ -55,6 +56,70 @@ exports.signup = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "User cannot be registered. Please try again.",
+    });
+  }
+};
+
+
+// Login controller for authenticating users
+exports.login = async (req, res) => {
+  try {
+    // Get email or number and password from request body
+    const { emailOrNumber, password } = req.body;
+
+    // Check if emailOrNumber or password is missing
+    if (!emailOrNumber || !password) {
+      // Return 400 Bad Request status code with error message
+      return res.status(400).json({
+        success: false,
+        message: `Please Fill up All the Required Fields`,
+      });
+    }
+
+    // Find user with provided email or number
+    const user = await User.findOne({
+      $or: [{ email: emailOrNumber }, { phoneNumber: emailOrNumber }],
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: `User is not Registered with Us. Please SignUp to Continue`,
+      });
+    }
+
+    // Compare Password
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign(
+        {
+          email: user.email,
+          id: user._id.toString(),
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h",
+        }
+      );
+
+      return res.status(200).json({
+        success: true,
+        token,
+        name: user.name,
+        message: `User Login Success`,
+      });
+    } else {
+      // Return 401 Unauthorized status code with error message
+      return res.status(401).json({
+        success: false,
+        message: `Password is incorrect`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    // Return 500 Internal Server Error status code with error message
+    return res.status(500).json({
+      success: false,
+      message: `Login Failure. Please Try Again`,
     });
   }
 };
