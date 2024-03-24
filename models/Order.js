d7e814374f20d305f34bd85a275361553a0bdbe1
Ubais88 <moubais2143@gmail.com>
2024-03-24 17:42:22 +0530

    order model added

---
+++ models/Order.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    orderTime: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
--- models/User.js
+++ models/User.js
@@ -40,6 +40,7 @@ const userSchema = new mongoose.Schema(
       }
     }]
   },
+  
   { timestamps: true }
 );
 
