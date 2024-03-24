c1e9851d1e4cd9bf4a63d29434a9e5ae562ad865
Ubais88 <moubais2143@gmail.com>
2024-03-24 12:48:40 +0530

    get add tocart route added

--- routes/Product.js
+++ routes/Product.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 
 // Import the required controllers and middleware functions
-const { getAllproducts, getProduct } = require("../controllers/Product");
+const { getAllproducts, getProduct, addToCart } = require("../controllers/Product");
 
 
 // Route for user createQuiz
 router.post("/allproducts", getAllproducts);
 router.get("/details/:productId", getProduct);
+router.post('/addtocart' , addToCart);
 
 
 // Export the router for use in the main application
